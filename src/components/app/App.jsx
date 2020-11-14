c81c6d76dcd27e64311edbcf826c3662a96ed90f
bwapes <benwaples@gmail.com>
2020-11-13 16:22:00 -0800

    auth parent

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Login from '../auth/Login';
+import { Auth } from '../auth/Auth';
 
 export default function App() {
   return (
-    <Login />
+    <Auth />
   );
 }
--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './Login';
 import SignUp from './SignUp';
-import styles from './AuthStyles.scss';
+import styles from './AuthStyles.css';
 
 export const Auth = () => {
+  const [displaySignUp, setDisplaySignUp] = useState(false);
+
+  const toggle = () => {
+    displaySignUp ? setDisplaySignUp(false) : setDisplaySignUp(true);
+  };
+
   return (
     <div>
-      <SignUp />
-      <Login />
+      {
+        displaySignUp ? <SignUp /> :  <Login />
+      }
+      <button onClick={toggle}>{displaySignUp ? 'Log In' : 'Sign Up'}</button>
     </div>
   );
 };
