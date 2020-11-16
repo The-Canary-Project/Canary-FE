ad509d63134659754dad48083beeeb676faba4d3
bwapes <benwaples@gmail.com>
2020-11-16 15:28:47 -0800

    styles and simple logic

--- src/components/calibrater/Tfcalibrater.jsx
+++ src/components/calibrater/Tfcalibrater.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { useEffect, useRef, useState } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import * as mobilenet from '@tensorflow-models/mobilenet';
@@ -11,10 +12,12 @@ export default function TfCalibrater() {
   const video = useRef();
   const [classifier, setClassifier] = useState();
   const [net, setNet] = useState();
-  const [result, setResult] = useState();
   const [feedback, setFeedback] = useState();
-  const [position, setPosition] = useState();
+  const [isVisible, setVisibility] = useState(false);
+  const [calibratedPositions, setCalibratedPositions] = useState([]);
   const dispatch = useDispatch();
+
+ 
   
   useEffect(async() => {
     const classifier = KNN.create();
@@ -24,8 +27,15 @@ export default function TfCalibrater() {
     const stream = await window.navigator.mediaDevices.getUserMedia({ video });
     video.current.srcObject = stream;
 
-    setInterval(async() => {
+    const training = setInterval(() => {
+      train('no answer');
+      console.log('machine trained');
+    }, 250);
+    setTimeout(() => {
+      clearInterval(training);
+    }, 3000);
 
+    setInterval(async() => {
       const image = tf.browser.fromPixels(video.current);
       const logits = net?.infer(image, 'conv_preds');
       const result = await classifier?.predictClass(logits);
@@ -37,7 +47,7 @@ export default function TfCalibrater() {
     }, 500);
 
 
-  }, [position]);
+  }, []);
 
   const train = name => {
     const image = tf.browser.fromPixels(video.current);
@@ -45,41 +55,58 @@ export default function TfCalibrater() {
     classifier.addExample(logits, name);
     logits.dispose();
     image.dispose();
-
   };
 
   const handleCalibrate = ({ target }) => {
+    setCalibratedPositions(state => state.push(target.name));
+    setVisibility(true);
     const training = setInterval(() => {
       train(target.name);
       console.log('machine trained');
     }, 250);
     setTimeout(() => {
       clearInterval(training);
-    }, 3000);
+      setVisibility(false);
+    }, 4500);
+  };
+
+  const handleAcceptFeedback = () => {
+    if(calibratedPositions.length === 4) {
+      dispatch(setNetState(net));
+      dispatch(setClassifierState(classifier));
+      alert('calibration model has been set');
+    } else {
+      // make message more indicative of which calibrations are needed
+      alert('calibration incomplete');
+    }
   };
 
   return (
     <>
-      <button name="a" onClick={handleCalibrate}>a</button>
-      <button name="b" onClick={handleCalibrate}>b</button>
-      <button name="c" onClick={handleCalibrate}>c</button>
-      <button name="d" onClick={handleCalibrate}>d</button>
-      <h2>position: {position}</h2>
-      <h2>feedback: {feedback}</h2>
-      <div className={styles.parent}>
-        <div className={styles.gridparent}>
-          <div className = {`${styles.topleftbox}`}>A</div>
-          <div className = {`${styles.toprightbox}`}>B</div>
-          <div className = {`${styles.bottomleftbox}`}>C</div>
-          <div className = {`${styles.bottomrightbox}`}>D</div>
-          
-
+      <div className={styles.upperCalibration}>
+        <img 
+          src="https://i0.wp.com/css-tricks.com/wp-content/uploads/2020/01/timer-progress-animated.gif?ssl=1" 
+          alt="timer" 
+          className={isVisible ? `${styles.visible}` : `${styles.notVisible}`} />
+        <div className={styles.parent}>
+          <div className={styles.gridparent}>
+            <div className = {`${styles.topleftbox} ${feedback === 'a' && styles.feedback}`}>A</div>
+            <div className = {`${styles.toprightbox} ${feedback === 'b' && styles.feedback}`}>B</div>
+            <div className = {`${styles.bottomleftbox} ${feedback === 'c' && styles.feedback}`}>C</div>
+            <div className = {`${styles.bottomrightbox} ${feedback === 'd' && styles.feedback}`}>D</div>
+          </div>
+          <video ref={video} autoPlay></video>
         </div>
-        <video ref={video} autoPlay></video>
-
-        
       </div>
-
+      <div className={styles.buttonParent}>
+        <div className={styles.calibrateButtons}>
+          <button name="a" onClick={handleCalibrate}>Calibrate A</button>
+          <button name="b" onClick={handleCalibrate}>Calibrate B</button>
+          <button name="c" onClick={handleCalibrate}>Calibrate C</button>
+          <button name="d" onClick={handleCalibrate}>Calibrate D</button>
+        </div>
+        <button onClick={handleAcceptFeedback} >Accept Calibrate</button>
+      </div>
     </>
 
 
