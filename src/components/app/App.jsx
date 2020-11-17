f5704ddd38faf88e9e5edc497a944465c37536a9
bwapes <benwaples@gmail.com>
2020-11-16 21:26:57 -0800

    timer component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,13 +4,12 @@ import { Auth } from '../auth/Auth';
 import store from '../../store';
 import TfCalibrater from '../calibrater/Tfcalibrater';
 import Chat from '../chat/Chat';
+import { Play } from '../play/Play';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Auth />
-      <TfCalibrater />
-      <Chat />
+      <Play />
     </Provider>
   );
 }
--- src/components/play/Play.jsx
+++ src/components/play/Play.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import { useSelector, useDispatch } from 'react-redux';
+import styles from './Play.css';
+import { feedbackElements } from '../../utils/styleContainers';
+import Timer from './Time';
 
 export const Play = () => {
+  const net = useSelector(state => state.net);
+  const classifier = useSelector(state => state.classifier);
+  const [feedback, setFeedback] = useState();
+  const [timer, setTimer] = useState(5);
+  const dispatch = useDispatch();
+
+  const video = useRef();
   
+  useEffect(async() => {
+    const stream = await window.navigator.mediaDevices.getUserMedia({ video });
+    video.current.srcObject = stream;
+
+    // const test = setInterval(() => {
+    //   setTimer(state => state - 1);
+    // }, 1000);
+    
+    // setTimeout(() => (clearInterval(test)), timer * 1000);
+
+    // setInterval(async() => {
+    //   const image = tf.browser.fromPixels(video.current);
+    //   const logits = net.infer(image, 'conv_preds');
+    //   const result = await classifier.predictClass(logits);
+    //   setFeedback(result.label);
+    //   logits.dispose();
+    //   image.dispose();
+    // }, 500);
+
+  }, []);
+
+
+
   return (
     <div>
-      
+      <Timer timer={timer} handleTimer={setTimer} />
+      <div className={styles.parent}>
+        <div className={styles.gridparent}>
+          {feedbackElements(styles, feedback)}
+        </div>
+        <video ref={video} autoPlay></video>
+      </div>
     </div>
   );
 };
