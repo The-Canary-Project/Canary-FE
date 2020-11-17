7d9b11b096bbc52ea07771cdb6e95c4a25bc9e90
Ben Waples <66393111+benwaples@users.noreply.github.com>
2020-11-16 16:37:05 -0800

    Calibrate (#2)
    * calibrate reducer
    
    * score reducer
    
    * chatroom and teacher actions
    
    * component started
    
    * update variable names
    
    * mirror video
    
    * styles and simple logic
    
    * update calibratedPostions
    
    * update white spacing
    
    Co-authored-by: Greg Mall <gregmall157@gmail.com>
    Co-authored-by: katiePDX <61799774+katiepdx@users.noreply.github.com>

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { createStore, compose } from 'redux';
-import reducers from './reducers';
+import reducer from './reducers/reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
-  reducers,
+  reducer,
   composeEnhancers()
 );
