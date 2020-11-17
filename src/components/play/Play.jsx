c64323986b37d97f192982853f92b7baf23bb56b
bwapes <benwaples@gmail.com>
2020-11-17 15:34:08 -0800

    correct answer display

--- src/components/play/Play.jsx
+++ src/components/play/Play.jsx
@@ -4,11 +4,17 @@ import { useSelector, useDispatch } from 'react-redux';
 import styles from './Play.css';
 import { feedbackElements } from '../../utils/styleContainers';
 import Timer from './Time';
+import { question1 } from './testQuestion';
+import { makeAnswers } from '../../utils/setPlay';
 
 export const Play = () => {
   const net = useSelector(state => state.net);
   const classifier = useSelector(state => state.classifier);
+  const [question, setQuestion] = useState(question1);
+  const [questionAssets, setQuestionAssets] = useState({});
   const [feedback, setFeedback] = useState();
+  const [result, setResult] = useState();
+  const [countdown, setCountdown] = useState();
   const [timer, setTimer] = useState(5);
   const dispatch = useDispatch();
 
@@ -17,32 +23,40 @@ export const Play = () => {
   useEffect(async() => {
     const stream = await window.navigator.mediaDevices.getUserMedia({ video });
     video.current.srcObject = stream;
+    // set incoming questions to question
+    setQuestionAssets(makeAnswers(question));
 
-    setInterval(async() => {
+    setCountdown(setInterval(async() => {
       const image = tf.browser.fromPixels(video.current);
       const logits = net.infer(image, 'conv_preds');
       const result = await classifier.predictClass(logits);
       setFeedback(result.label);
       logits.dispose();
       image.dispose();
-    }, 500);
+    }, 500));
 
   }, []);
 
   // evaluate game results here and update socket and state score
   if(timer === 0) {
+    clearInterval(countdown);
+    console.log(questionAssets);
+    console.log((feedback === questionAssets.correctAnswer) ? 'bingo' : 'wrong');
     console.log('evaluate gamne');
   }
 
   return (
     <div className={styles.play}>
       <Timer timer={timer} handleTimer={setTimer} />
+      {questionAssets.answerElements}
+      {result}
       <div className={styles.parent}>
         <div className={styles.gridparent}>
           {feedbackElements(styles, feedback)}
         </div>
         <video ref={video} autoPlay></video>
       </div>
+      <h3>{question.text}</h3>
     </div>
   );
 };
--- src/utils/setPlay.js
+++ src/utils/setPlay.js
@@ -1,10 +1,54 @@
-export const makeAnswers = (type, answers) => {
-  // make answer elements depending on the question type
-  if(type === 'multipleChoice') {
-    // map through answers and create 4 elements
+import React from 'react';
 
-  } else {
-    // map through answers and create 2 elements
-  }
-  //
+export const makeAnswers = (question) => {
+  // make answer elements depending on the question type
+  // find text of the correct answer
+  return (question.type === 'multipleChoice') ? 
+    multipleChoiceAssets(question) :
+    trueFalseAssets(question);
+
 };
+
+
+function multipleChoiceAssets(question) {
+  const options = ['a', 'b', 'c', 'd'];
+  const answerElements = question.answers.map((answer, i) => {
+    return (
+      <div key={i}>
+        <h3>{options.i}</h3>
+        <p>answer: {answer.text}</p>
+        {answer.imageUrl && 
+        <img src={answer.imageUrl} alt={answer.text}/>} 
+      </div>
+    );
+  });
+  // find the text of the correct answer
+  const correctAnswerIndex = question.answers.map(answer => answer.isCorrect).indexOf(true);
+
+  const correctAnswer = options[correctAnswerIndex];
+  return {
+    answerElements,
+    correctAnswer
+  };
+}
+
+function trueFalseAssets(question) {
+  const answerElements = question.answers.map((answer, i) => {
+    return (
+      <div key={i}>
+        <p>{answer.text}</p>
+        {answer.imageUrl && 
+        <img src={answer.imageUrl} alt={answer.text}/>} 
+      </div>
+    );
+  });
+  
+  // find the text of the correct answer
+  const correctAnswerIndex = question.answers.find(answer => answer.isCorrect);
+  const correctAnswer = ['a', 'b'][correctAnswerIndex];
+
+  return {
+    answerElements,
+    correctAnswer
+  };
+}
