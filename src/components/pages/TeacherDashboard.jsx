350a71d69934ecf87d022a8be70de2bc17eab26d
Thomas R Stussi <tstussi06@gmail.com>
2020-11-17 14:26:01 -0800

    Socket provider (#8)
    * added socket provider to student and teacher pages, refactored chatform and chatlist socket events
    
    * provider debugging
    
    * local storage solution for chat username bug implemented
    
    * tweaks

--- src/components/pages/TeacherDashboard.jsx
+++ src/components/pages/TeacherDashboard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { SocketProvider } from '../../provider/socketProvider';
+import Chat from '../chat/Chat';
 
 export default function TeacherDashboard() {
   return (
     <div>
-      Teacher Dashboard
+      <SocketProvider>
+        Teacher Dashboard
+        <Chat />
+      </SocketProvider>
     </div>
-  )
+  );
 }
---
+++ src/provider/socketProvider.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import io from 'socket.io-client';
+
+const SocketContext = React.createContext();
+
+export const SocketProvider = ({ children }) => {
+  const socket = io(process.env.SOCKET_URL);
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+export const useSocket = () => useContext(SocketContext);
