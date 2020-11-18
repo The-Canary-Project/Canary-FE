66bdb3996eb69d4aafaa3132281f2dcb09936ac1
Katie <61799774+katiepdx@users.noreply.github.com>
2020-11-17 16:21:20 -0800

    Teacher dashboard (#11)
    * calibrate reducer
    
    * score reducer
    
    * chatroom and teacher actions
    
    * component started
    
    * update variable names
    
    * chat file tree added
    
    * add chat state management
    
    * add empty chat message check
    
    * Monday Features (#4)
    
    * initial commit/bootstrap
    
    * initial README
    
    * docs folder
    
    * signup component and route
    
    * signup form tested
    
    * login test
    
    * styles file
    
    * auth parent
    
    * basic form styles
    
    * signup reducer
    
    * login reducer
    
    * update input and button styles
    
    * change form toggle styles
    
    * update PR
    
    * update test with provder
    
    * Calibrate (#2)
    
    * calibrate reducer
    
    * score reducer
    
    * chatroom and teacher actions
    
    * component started
    
    * update variable names
    
    * mirror video
    
    * styles and simple logic
    
    * update calibratedPostions
    
    * update white spacing
    
    Co-authored-by: Greg Mall <gregmall157@gmail.com>
    Co-authored-by: katiePDX <61799774+katiepdx@users.noreply.github.com>
    
    * monday-features (#3)
    
    * calibrate reducer
    
    * score reducer
    
    * chatroom and teacher actions
    
    * component started
    
    * update variable names
    
    * chat file tree added
    
    * add chat state management
    
    * add empty chat message check
    
    Co-authored-by: bwapes <benwaples@gmail.com>
    Co-authored-by: Greg Mall <gregmall157@gmail.com>
    Co-authored-by: Thomas Stussi <tstussi06@gmail.com>
    
    Co-authored-by: Thomas Stussi <tstussi06@gmail.com>
    Co-authored-by: Greg Mall <gregmall157@gmail.com>
    Co-authored-by: bwapes <benwaples@gmail.com>
    Co-authored-by: Ben Waples <66393111+benwaples@users.noreply.github.com>
    
    * display all prompts on teacher dashboard
    
    Co-authored-by: bwapes <benwaples@gmail.com>
    Co-authored-by: Greg Mall <gregmall157@gmail.com>
    Co-authored-by: Thomas Stussi <tstussi06@gmail.com>
    Co-authored-by: Ben Waples <66393111+benwaples@users.noreply.github.com>

--- src/components/pages/TeacherDashboard.jsx
+++ src/components/pages/TeacherDashboard.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { getAllPrompts } from '../../services/PromptsService'
+import PromptList from '../teacher-dashboard/PromptList'
 import { SocketProvider } from '../../provider/socketProvider';
 import Chat from '../chat/Chat';
+import { useSocket } from '../../provider/socketProvider'
 
 export default function TeacherDashboard() {
+  const [prompts, setPrompts] = useState([])
+
+  useEffect(async () => {
+    const APIPrompts = await getAllPrompts()
+
+    setPrompts(APIPrompts)
+  }, [])
+
+  const handleClick = ({target}) => {
+    const socket = useSocket()
+    socket.emit('SEND_QUESTION', target.value)
+  }
+
   return (
     <div>
       <SocketProvider>
-        Teacher Dashboard
+        <h1>Teacher Dashboard</h1>
+
+        <section>
+          <PromptList promptList={prompts} handleClick={handleClick} />
+        </section>
+
         <Chat />
       </SocketProvider>
     </div>
