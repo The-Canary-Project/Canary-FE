ac8a2fdf7371b9fdbf2f1e0faf7b393f1b5d267a
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-11-19 14:24:04 -0800

    move chat input to bottom

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ChatForm from './ChatForm';
 import ChatList from './ChatList';
+import styles from './Chat.css'
 
 export default function Chat() {
   return (
-    <div>
+    <div className={styles.chatContainer}>
       <ChatList />
       <ChatForm />
     </div>
